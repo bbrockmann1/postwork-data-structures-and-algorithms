bcbc591316b0ad6dea29dd6a6c2ffe802bc2412f
Bo Brockmann <bbrockmann1@gmail.com>
2023-02-01 13:19:01 -0700

    reversed strings

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
-  // type your code here
+  let reversed = "";
+
+  for (let i = str.length - 1; i > -1; --i) {
+    reversed = reversed + str[i];
+  }
+
+  return reversed;
 }
 
 if (require.main === module) {
@@ -11,9 +17,15 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'ydwoh'")
+  console.log("=>", reverseString("howdy"))
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+  //initialize reversed variable, iterate through every letter in the string backwards, add each letter to the initialized reversed variable
 // And a written explanation of your solution
